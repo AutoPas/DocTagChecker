bacaa2b09aa4100cd07f390a74fb82db229da0a8
FG-TUM <f.gratl@tum.de>
2024-01-15 17:29:16 +0100

    some test version

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
+// import * as exec from '@actions/exec'
 import * as github from '@actions/github'
 
 /**
@@ -8,16 +8,34 @@ import * as github from '@actions/github'
  */
 export async function run(): Promise<void> {
   try {
+    // Input parsing
     // Get directories as arrays. Split at any amount of white space characters.
     const dirs: string[] = core.getInput('userDocsDirs').split(/\s+/)
     core.info(`User doc directories: ${dirs}`)
-
     const baseBranch: string = core.getInput('baseBranch')
     core.info(`Base branch: ${baseBranch}`)
+    // const ghToken: string = core.getInput('githubToken')
+
+    const ghToken = process.env.GITHUB_TOKEN
+    const owner = process.env.GITHUB_REPOSITORY.split('/')[0]
+    const repo = process.env.GITHUB_REPOSITORY.split('/')[1]
+    core.info('process.env.GITHUB_REPOSITORY: ${process.env.GITHUB_REPOSITORY}')
+    const pull_number = parseInt(process.env.GITHUB_PULL_REQUEST_NUMBER, 10)
+    core.info('process.env.GITHUB_PULL_REQUEST_NUMBER: ${process.env.GITHUB_PULL_REQUEST_NUMBER}')
+    core.info('process.env: ${process.env}')
+    const octokit = github.getOctokit(ghToken)
+    // Get the list of changed files in the pull request
+    const response = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number,
+    })
+    // Extract file names from the response
+    const changedFiles = response.data.map((file) => file.filename);
+    core.info(`changed files: ${changedFiles}`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('warnings', "NO WARNINGS")
-
+    core.setOutput('warnings', 'NO WARNINGS')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
