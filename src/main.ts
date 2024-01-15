e5464c85a1b84608f66df40689763a134946ac5d
FG-TUM <f.gratl@tum.de>
2024-01-15 23:43:03 +0100

    debug messages

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,27 @@ export async function run(): Promise<void> {
     core.info(`Base branch: ${baseBranch}`)
     // const ghToken: string = core.getInput('githubToken')
 
+    core.info(`FOOO 1`)
     const ghToken = process.env.GITHUB_TOKEN
+    core.info(`FOOO 2`)
     const owner = process.env.GITHUB_REPOSITORY.split('/')[0]
+    core.info(`FOOO 3`)
     const repo = process.env.GITHUB_REPOSITORY.split('/')[1]
-    core.info('process.env.GITHUB_REPOSITORY: ${process.env.GITHUB_REPOSITORY}')
+    core.info(`FOOO 4`)
+    core.info(`process.env.GITHUB_REPOSITORY: ${process.env.GITHUB_REPOSITORY}`)
+    core.info(`FOOO 5`)
     const pull_number = parseInt(process.env.GITHUB_PULL_REQUEST_NUMBER, 10)
-    core.info('process.env.GITHUB_PULL_REQUEST_NUMBER: ${process.env.GITHUB_PULL_REQUEST_NUMBER}')
-    core.info('process.env: ${process.env}')
+    core.info(`FOOO 6`)
+    core.info(`process.env.GITHUB_PULL_REQUEST_NUMBER: ${process.env.GITHUB_PULL_REQUEST_NUMBER}`)
+    core.info(`FOOO 7`)
+    core.info(`process.env: ${process.env}`)
+    core.info(`FOOO 8`)
+    core.info(`process.env: ${process.env}`)
+    core.info(`FOOO 9`)
+    core.info(`process.env: ${process.env}`)
+    core.info(`FOOO 10`)
     const octokit = github.getOctokit(ghToken)
+    core.info(`FOOO 11`)
     // Get the list of changed files in the pull request
     const response = await octokit.rest.pulls.listFiles({
       owner,
@@ -37,7 +50,9 @@ export async function run(): Promise<void> {
     // Set outputs for other workflow steps to use
     core.setOutput('warnings', 'NO WARNINGS')
   } catch (error) {
+    core.info(`CATCH 1`)
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
+  core.info(`DONE`)
 }
