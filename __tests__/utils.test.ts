7362a2065121f3a9fdd1e4ecdf990b5f1a96d01c
FG-TUM <f.gratl@tum.de>
2024-01-23 23:07:59 +0100

    added missing utils tests

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -2,7 +2,12 @@
  * Unit tests for src/utils.ts
  */
 
-import { getUrlToFile, findFileByName, assertNonNull } from '../src/utils'
+import {
+  getUrlToFile,
+  getUrlToChanges,
+  findFileByName,
+  assertNonNull
+} from '../src/utils'
 import * as github from '@actions/github'
 import { expect } from '@jest/globals'
 
@@ -95,5 +100,36 @@ describe('utils.ts context dependent', () => {
     // Calling the function and testing the result
     const result = getUrlToFile(filePath)
     expect(result).toBe(expectedUrl)
+  })
+
+  it('getUrlToChanges(): Test URL builder', () => {
+    // Test value
+    const filePath = 'mock/path.lol'
+    // Computed in bash via
+    // echo -n mock/path.lol | sha256sum
+    const filePathHash =
+      'ee6a878745ced544a2a8900685af1dbd9ddd5c775f980dd212bfd527ac6ab32b'
+
+    const errorString = 'fail'
+    const expectedUrl =
+      `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/pull/${github.context.payload.pull_request?.number}/files#diff-${filePathHash}` ??
+      errorString
+
+    expect(expectedUrl).not.toBe(errorString)
+
+    // Calling the function and testing the result
+    const result = getUrlToChanges(filePath)
+    expect(result).toBe(expectedUrl)
+  })
+
+  it('getUrlToChanges(): Test missing PR number', () => {
+    // Test value
+    const filePath = 'mock/path.lol'
+
+    // Change the mock value, because according to the specification this state is possible.
+    github.context.payload.pull_request = undefined
+
+    // Calling the function with this context should throw an error.
+    expect(() => getUrlToChanges(filePath)).toThrow()
   })
 })
